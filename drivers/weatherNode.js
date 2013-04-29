ba758ad37ee144deaca5b336ee43c1f166fdfae3
Lars Toft Jacobsen <lars@boxed.dk>
2013-04-29 20:52:43 +0200

    small unit adjustments

--- drivers/weatherNode.js
+++ drivers/weatherNode.js
@@ -6,21 +6,21 @@ module.exports = {
         tempi: {
             title: 'Temperature indoor',
             unit: '°C',
-            scale: 0,
+            scale: 1,
             min: -50,
             max: 50
         },
         tempo: {
             title: 'Temperature outdoor',
             unit: '°C',
-            scale: 0,
+            factor: 0.0625,
             min: -50,
             max: 50
         },
         press: {
             title: 'Pressure',
             unit: 'hPa',
-            scale: 0,
+            scale: 2,
             min: 800,
             max: 1200
         },
@@ -34,15 +34,11 @@ module.exports = {
     feed: 'rf12.packet',
 
     decode: function (raw, callback) {
-        ti = raw.readInt16LE(1) / 10.0;
-        to = raw.readInt16LE(3) / 16.0;
-        p = raw.readInt32LE(5) / 100.0;
-        lb = raw[9] & 1;
         callback({
-            tempi: ti,
-            tempo: to,
-            press: p,
-            lobat: lb
+            tempi: raw.readInt16LE(1),
+            tempo: raw.readInt16LE(3),
+            press: raw.readInt32LE(5),
+            lobat: raw[9] & 1
         });
     }
 
