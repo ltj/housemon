998a40b4fb1d47dfa0bf35a72395e054ae4123c4
Lars Toft Jacobsen <lars@boxed.dk>
2013-05-11 20:18:23 +0200

    added powernode

--- briqs/nodeMap-local.coffee
+++ briqs/nodeMap-local.coffee
@@ -3,11 +3,13 @@ exports.rf12nodes =
     2: 'weatherNode'
     3: 'roomNodeLight'
     4: 'roomNodeLight'
+    5: 'powerNode'
     22: 'radioBlip'
 
 exports.locations =
   'RF12:212:2': title: 'Kitchen'
   'RF12:212:3': title: 'Living room'
   'RF12:212:4': title: 'Bathroom'
+  'RF12:212:5': title: 'Power meter'
   'BATT-1': title: 'JNu v3 test'
 
---
+++ drivers/powerNode.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+    announcer: 102,
+
+    descriptions: {
+        power: {
+            title: 'Power',
+            unit: 'W',
+            min: 0,
+            max: 36000
+        },
+        elapsed: {
+            title: 'Elapsed Wh',
+            unit: 'Wh',
+            min: 0,
+            max: 100
+        },
+        lobat: {
+            title: 'Low battery',
+            min: 0,
+            max: 1
+        }
+    },
+
+    feed: 'rf12.packet',
+
+    decode: function (raw, callback) {
+        callback({
+            power: raw.readInt16LE(1),
+            elapsed: raw.readInt16LE(3),
+            lobat: raw[5] & 1
+        });
+    }
+
+};
